5cc7a53176a193c9d6c392cdc9fdc013cd1d4520
Francisco Viana <franciscoviana862@gmail.com>
2019-10-02 07:44:35 -0300

    create backend

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://omnistack:omnistack@omnistack-shard-00-00-n7bzn.mongodb.net:27017,omnistack-shard-00-01-n7bzn.mongodb.net:27017,omnistack-shard-00-02-n7bzn.mongodb.net:27017/test?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// GET , POST , PUT, DELETE
+
+// req.query = Acessar query parms (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
